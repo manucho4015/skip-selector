ff28256404121cfb58387d39ff0f48c4837a14be
Emmanuel Ochieng <eochieng16@gmail.com>
2025-06-06 11:34:28 +0300

    skip grid component

--- src/components/SkipGrid.tsx
+++ src/components/SkipGrid.tsx
@@ -1,8 +1,25 @@
+// components
+import SkipCard from "./SkipCard"
 
-const SkipGrid = () => {
+// custom types 
+import type { Skip } from "../types/skip"
+
+interface SkipGridProps {
+    skips: Skip[]
+    selectedId: number
+    onSelect: (id: number) => void
+}
+const SkipGrid = ({ skips, selectedId, onSelect }: SkipGridProps) => {
     return (
-        <div>
-
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {skips.map((skip) => (
+                <SkipCard
+                    key={skip.id}
+                    skip={skip}
+                    selected={selectedId === skip.id}
+                    onSelect={onSelect}
+                />
+            ))}
         </div>
     )
 }
